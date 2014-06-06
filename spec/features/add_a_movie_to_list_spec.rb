284b50f0cd4e042d11478cb0725332fb25ab065d
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-06-06 10:57:58 -0700

    Created first complete feature test

--- spec/features/add_a_movie_to_list_spec.rb
+++ spec/features/add_a_movie_to_list_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
 
-scenario 'user adds a movie to a new list' do
 
-  user = current_user
-  list = List.new("movie list")
-  movie = Movie.new("The Best Movie Ever")
+feature 'create a new list', %q(
+  As a user,
+  I want to be able to create a list,
+  So I can add movies to it
+  ) do
+
+  scenario 'user adds a movie to a new list' do
+
+    user = FactoryGirl.create(:user)
+
+    sign_in_as(user)
+    visit root
+    click_on("Create a new list")
+
+    list = FactoryGirl.build(:list)
+    fill_in "Title", with: list.title
+    fill_in "Description", with: list.description
+    click_on "Create List"
+    expect(page).to have_content("List created!")
+  end
 end
